aea8f771d4c2c9a1ef3ce80102ddeb5bcc821c64
miUsu <josekber1@gmail.com>
2025-04-09 18:57:18 -0500

    Esto es un cambio

---
+++ src/githubestructuras/GitHubEstructuras.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package githubestructuras;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class GitHubEstructuras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola GitHub");
+        System.out.println("Esto es un cambio");
+    }
+    
+}
